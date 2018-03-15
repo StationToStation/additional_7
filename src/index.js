c52e3c31329d8fd61a223d70925301a847f5244c
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-03-15 17:53:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = function solveSudoku(puzzle) {
+    let unsolved = 1;
+
+    function Check(compared, puzzle, i, j) {
+        //Проверяет, можно ли поставить число
+
+        //в квадрате
+        let row = Math.floor(i / 3) * 3;
+        let col = Math.floor(j / 3) * 3;
+        for (let k = row; k < row + 3; k++)
+            for (let n = col; n < col + 3; n++)
+                if (compared == puzzle[k][n]) return false;
+
+        //в строке
+        for (let k = 0; k < puzzle[i].length; k++)
+            if (compared == puzzle[i][k]) return false;
+
+        // в столбце
+        for (let k = 0; k < puzzle.length; k++)
+            if (compared == puzzle[k][j]) return false;
+
+        return true;
+    }
+
+    function RecSolve(puzzle, index) {
+        if (index > 80) {
+            unsolved = 0;
+            return;
+        }
+        let i = Math.floor(index / 9),
+            j = index % 9;
+        if (puzzle[i][j] !== 0)
+            return RecSolve(puzzle, index + 1);
+
+
+        for (let variant = 1; variant < 10; variant++) {
+            if (Check(variant, puzzle, i, j)) {
+                puzzle[i][j] = variant;
+                RecSolve(puzzle, index + 1);
+            }
+        }
+        if (unsolved) puzzle[i][j] = 0;
+    }
+    RecSolve(puzzle, 0);
+    return puzzle;
 }
